d362b5523d51e7e76e09781d733ee12b8018003b
Szymon Gałecki <szymongalecki@gmail.com>
2023-06-29 16:05:31 +0200

    modified examples

--- fib.py
+++ fib.py
@@ -1,4 +1,7 @@
 import numpy
+import sys
+
+sys.set_int_max_str_digits(0)
 
 def fib(n):
     if n >= 0:
@@ -20,5 +23,6 @@ def fib(n):
     # Return value of Fn
     return F[0][0]
 
-
-print(f"F_20_000: \n{fib(20_000)}")
+print(f"F_100: \n{str(fib(100))}\n")
+print(f"F_1,000: \n{str(fib(1_000))[:100]}\n")
+print(f"Last 100 digits of F_1,000,000: \n{str(fib(1_000_000))[:100]}")
